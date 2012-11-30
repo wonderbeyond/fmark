fb0f0de8cdc39a600e4c9d934d776499683f860a
Wanchao Zhang <wonderbeyond@gmail.com>
2012-11-30 09:11:00 +0800

    add alias_short.sh

--- alias.sh
+++ alias.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-# Author: Wonder
 # Description: alias for fmark
-
 alias fmadd="fmark add"
 alias fmget="fmark get"
 alias fmls="fmark ls"
---
+++ alias_short.sh
@@ -0,0 +1,7 @@
+# Description: alias for fmark
+alias fma="fmark add"
+alias fmg="fmark get"
+alias fmls="fmark ls"
+alias fmll="fmark ls -l"
+alias fmd="fmark del"
+alias fme="fmark get -e"
